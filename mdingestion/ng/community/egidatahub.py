f295c5aa4e24b444f6be564e6a7bf3cea8b7576d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2020-06-29 11:54:13 +0200

    added reader and writer

--- mdingestion/ng/community/egidatahub.py
+++ mdingestion/ng/community/egidatahub.py
@@ -1,7 +1,15 @@
-from ..schema import DublinCore
+from ..reader import DublinCoreReader
 
-class EGIDatahubDublinCore(DublinCore):
 
-    @property
-    def publisher(self):
-        return ['EGI Datahub']
+class EGIDatahubDublinCore(DublinCoreReader):
+    def update(self, doc):
+        doc.publisher = ['EGI Datahub']
+        doc.doi = self.doi(doc)
+
+    def doi(self, doc):
+        dois = [id for id in self.parser.find('identifier') if id.startswith('http://hdl.handle.net')]
+        if dois:
+            url = dois[0]
+        else:
+            url = ''
+        return url
